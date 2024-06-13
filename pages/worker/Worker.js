82d3c7df2c1366de8b335b67f1230654497f5fec
Shadab <alamshadab9876543210@gmail.com>
2024-06-13 21:24:26 +0530

    removed email

--- pages/worker/Worker.js
+++ pages/worker/Worker.js
@@ -163,18 +163,6 @@ const Worker = () => {
                   <option value="Laundry work">Laundry work</option>
                 </select>
               </div>
-              <div className="relative mb-4">
-                <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                <input
-                
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
               <button
                 type="submit"
                 className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
